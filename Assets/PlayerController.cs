846fda926906300d83561c9d695316247b6f2719
Juul_Cox <juulcox00@gmail.com>
2019-05-16 10:18:09 +0200

    ffg

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,24 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Vector3> waypoint;
+
     void Start()
     {
+        waypoint = new List<Vector3>();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
         foreach (var touch in Input.touches)
         {
-            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.nearClipPlane));
-            transform.position = pos;
-            Debug.Log(pos);
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(touch.position), out hitInfo);
+            if (hit)
+            {
+                HighlightGround(hitInfo);
+            }
+            if(touch.phase == TouchPhase.Ended)
+            {
+                MovePlayer();
+                //waypoint.Clear();
+            }
         }
         if (Input.GetMouseButton(0))
         {
-
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit)
+            {
+                HighlightGround(hitInfo);
+            }
         }
+        if (Input.GetMouseButtonUp(0))
+        {
+            MovePlayer();
+            //waypoint.Clear();
+        }
+    }
+
+    void HighlightGround(RaycastHit hitInfo)
+    {
+        if (!hitInfo.transform.gameObject.GetComponent<GroundBlock>().highlight)
+        {
+            hitInfo.transform.gameObject.GetComponent<GroundBlock>().highlight = true;
+            Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+            hitInfo.transform.gameObject.GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0);
+            waypoint.Add(hitInfo.transform.position);
+        }
+    }
+    void MovePlayer()
+    {
+
     }
 }
