02fa425c9c78e0cdfa153b4fd51ddc6f7fd043ed
Yanmoone <yanuhue@hotmail.com>
2019-06-11 12:03:50 +0200

    Artifact made visual

--- Assets/Scripts/Terrain scripts/MapManager.cs
+++ Assets/Scripts/Terrain scripts/MapManager.cs
@@ -4,32 +4,35 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    
+
     public Map map1;
     public Map map2;
     public Texture2D texture;
     public GameObject[] groundPrefabs;
     public GameObject[] artifactPrefabs;
 
+    public GameObject tempArtifact;
+
     private static MapManager instance = null;
-     
+
     public static MapManager Instance
     {
         get
-        { 
-            return instance; 
+        {
+            return instance;
         }
     }
-     
+
     private void Awake()
     {
-        if (instance != null && instance != this) 
+        
+        if (instance != null && instance != this)
         {
             Destroy(this.gameObject);
         }
- 
+
         instance = this;
-        DontDestroyOnLoad( this.gameObject );
+        DontDestroyOnLoad(this.gameObject);
     }
 
     void Start()
@@ -37,14 +40,14 @@ public class MapManager : MonoBehaviour
         map1 = new Map();
         map1.GenerateMap();
         InstantiateMap(map1);
-        
+
     }
 
     void InstantiateMap(Map map)
     {
         int r = 0;
         GameObject parent = new GameObject("Map");
-        
+
         for (int y = 0; y < map.terrainHeight; y++)
         {
             r++;
@@ -53,10 +56,15 @@ public class MapManager : MonoBehaviour
             for (int x = 0; x < map.terrainWidth; x++)
             {
                 Material groundTile = new Material(Shader.Find("Standard"));
-                texture = Resources.Load<Texture2D>("R"+(r).ToString()+" T"+(x + 1).ToString());
+                texture = Resources.Load<Texture2D>("R" + (r).ToString() + " T" + (x + 1).ToString());
                 groundTile.SetTexture("_MainTex", texture);
                 map1.ground[x, y].groundObject.GetComponent<MeshRenderer>().material = groundTile;
-                map1.groundGameObjects[x,y] = Instantiate(map.ground[x, y].groundObject,new Vector3Int(map.ground[x, y].position.x, map.ground[x, y].position.y,0),new Quaternion (0,0,90,0),parent.transform) as GameObject;
+                map1.groundGameObjects[x, y] = Instantiate(map.ground[x, y].groundObject, new Vector3Int(map.ground[x, y].position.x, map.ground[x, y].position.y, 0), new Quaternion(0, 0, 90, 0), parent.transform) as GameObject;
+
+                if (map.ground[x, y] is ArtifactGround)
+                {
+                    Instantiate(tempArtifact, new Vector3(map.ground[x, y].position.x, map.ground[x, y].position.y, -0.7f), Quaternion.identity);
+                }
             }
         }
     }
