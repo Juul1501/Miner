4774bdc24ec11360e49a47ead5d609ee57bfe100
Juul_Cox <juulcox00@gmail.com>
2019-06-12 09:33:24 +0200

    started working on puzzle game

--- Assets/Scripts/Terrain scripts/MapManager.cs
+++ Assets/Scripts/Terrain scripts/MapManager.cs
@@ -10,6 +10,9 @@ public class MapManager : MonoBehaviour
     public Texture2D texture;
     public GameObject[] groundPrefabs;
     public GameObject[] artifactPrefabs;
+    public Sprite[] sprites;
+    public Queue<ArtifactPiece> aftifacts;
+    public Material groundMat;
 
     private static MapManager instance = null;
      
@@ -53,8 +56,11 @@ public class MapManager : MonoBehaviour
             for (int x = 0; x < map.terrainWidth; x++)
             {
                 Material groundTile = new Material(Shader.Find("Standard"));
+                groundTile.EnableKeyword("_NORMALMAP");
                 texture = Resources.Load<Texture2D>("R"+(r).ToString()+" T"+(x + 1).ToString());
+                Texture2D normal = Resources.Load<Texture2D>("kartonnormal");
                 groundTile.SetTexture("_MainTex", texture);
+                groundTile.SetTexture("_BumpMap", normal);
                 map1.ground[x, y].groundObject.GetComponent<MeshRenderer>().material = groundTile;
                 map1.groundGameObjects[x,y] = Instantiate(map.ground[x, y].groundObject,new Vector3Int(map.ground[x, y].position.x, map.ground[x, y].position.y,0),new Quaternion (0,0,90,0),parent.transform) as GameObject;
             }
