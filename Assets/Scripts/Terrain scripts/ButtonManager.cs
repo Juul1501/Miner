4a9a4528f927a43100e54db76231572a46aaf164
Juul_Cox <juulcox00@gmail.com>
2019-06-19 19:01:35 +0200

    commit working on puzzle game

---
+++ Assets/Scripts/Terrain scripts/ButtonManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonManager : MonoBehaviour
+{
+    private static ButtonManager instance;
+
+    public static ButtonManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
